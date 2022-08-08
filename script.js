4fedb9751bda1104e3443d27f09e66cc39571e4f
Daniel <dnhandelman@gmail.com>
2022-08-08 11:00:21 -0700

    Adding JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function promptMe()
+{
+ let button1 = prompt("Do you have a soul?");
+ if (button1 == ("yes")) {alert("Congratulations, please enjoy."), 
+    window.location = "/americanspirits.html";
+} else {
+    alert("Entrance denied.");
+    }
+} 
+
